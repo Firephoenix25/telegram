057360658b26faed1f5973b60b482d4a3a01f93e
Kayo <kayodionizio@gmail.com>
2024-09-29 02:37:09 +0200

    tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,14 @@ export default function HomePage() {
           <div>
             <h1>Hello, {userData.first_name}!</h1>
             <p>Your user ID is {userData.id}.</p>
+            <p>Your username is {userData.username}.</p>
+            <p>Your photo URL is {userData.photo_url}.</p>
+            <p>Your language code is {userData.language_code}.</p>
+            <p>Your is_premium is {userData.is_premium}.</p>
+            <p>Your is_bot is {userData.is_bot}.</p>
+            <p>Your first_name is {userData.first_name}.</p>
+            <p>Your last_name is {userData.last_name}.</p>
+
           </div>
         )
           :
